a81c8d0b1c4d9b3291f78f4e479394800eb32c24
NickTTD <nico-diaz@hotmail.es>
2023-03-17 08:31:37 -0300

    ae?

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("hi");
-        System.out.println("hiiii");
+        System.out.println("test");
     }
 }
