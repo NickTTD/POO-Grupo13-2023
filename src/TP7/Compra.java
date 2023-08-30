12376acb5cfc10c9b11594d1cbbed2c4dc9568ac
NickTTD <nico-diaz@hotmail.es>
2023-08-30 20:30:59 -0300

    Co-authored-by: Juan Patricio Michel <thatduckwiththeglasses@users.noreply.github.com>

---
+++ src/TP7/Compra.java
@@ -0,0 +1,9 @@
+package TP7;
+import java.time.LocalDate;
+//*De la Compra se conoce el Cliente que la realizo, la fecha y el Detalle de la Compra (Lineas
+//*de Compra).
+public class Compra {
+    private cliente Cliente;
+    private fecha fecha;
+    private lineadecompra;
+}
